4cecd5b5bccc5fd431aecbd2b74fd9f51dfa7352
Haozhe Li <hao-zhe.li@outlook.com>
2025-02-14 11:47:01 -0600

    Add request origin validation to enhance security

--- app.py
+++ app.py
@@ -6,14 +6,27 @@ import time
 import os
 
 is_dev = os.environ.get("DEV", 0)
+allowed_hosts = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 app = Flask(__name__)
 search = AISearch()
 
+def request_come_from(request):
+    """
+    Check if the request comes from the same domain
+    Returns: bool
+    """
+    referer = request.headers.get('Referer', '')
+    origin = request.headers.get('Origin', '')
+    # make sure the referer is in the allowed hosts and the origin is in the allowed hosts
+    if any([host in referer for host in allowed_hosts]) and any([host in origin for host in allowed_hosts]):
+        return True
+    return False
 
 @app.route("/")
 async def index():
     return render_template("index.html")
+
 
 @app.route("/getsuggestion")
 async def getsuggestion():
@@ -24,6 +37,8 @@ async def getsuggestion():
 @app.route("/search", methods=["POST"])
 async def main():
     try:
+        if not request_come_from(request):
+            raise Exception("Request not allowed")
         query = request.json["query"]
         mode = request.json["mode"]
         title = f"""# {query}\n\n------\n\n
@@ -45,7 +60,11 @@ async def main():
         result = title + result["answer"] + footer
         return jsonify({"result": result})
     except Exception as e:
-        return jsonify({"result": "Hi I'm Omni, but something went wrong! Could you please try again?"})
+        return jsonify(
+            {
+                "result": "Hi I'm Omni, but something went wrong! Could you please try again?"
+            }
+        )
 
 
 if __name__ == "__main__":
